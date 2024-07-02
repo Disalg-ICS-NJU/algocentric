99a63ddcc2c7a28449099bf4eac04a4679be870b
Jingbo Zhai <201240105@smail.nju.edu.cn>
2024-07-02 15:51:11 +0800

    修改了测试框架，现在能生成更复杂的带负权边的样例

--- dijkstra/generator.py
+++ dijkstra/generator.py
@@ -18,17 +18,18 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-
 from random import randint
-from random import choice
 from random import sample
+from random import shuffle
+import copy
 import argparse
+from bellmanford import bellman_ford
 
 
 
 def get_random_graph(node_num: int, edge_num: int, min_w: int, max_w: int) \
         -> list:
-    '''获取随机生成的非负权权无向连通图
+    '''获取随机生成的非负权有向连通图
 
     Args:
         node_num(int): 图的节点数
@@ -41,54 +42,54 @@ def get_random_graph(node_num: int, edge_num: int, min_w: int, max_w: int) \
     '''
     nodes = list(range(node_num))
     edges = [[0 for _ in range(node_num)] for _ in range(node_num)]
-    edge_size=0
+    edge_size = 0
     node_pair = set()
-    used_nodes = set()
-    unused_nodes = set(nodes)
 
-    # 先添加n-1条边, 保证图连通
-    while edge_size < node_num-1:
-        if edge_size == 0:  # 生成第一条边
-            node_1 = randint(0, node_num-2)
-            node_2 = randint(node_1+1, node_num-1)
-            used_nodes = used_nodes.union((node_1, node_2))
-            unused_nodes = unused_nodes-{node_1, node_2}
-            edges[node_1][node_2]=randint(min_w, max_w)
-            node_pair.add((node_1, node_2))
-            edge_size += 1
-        else:  # 生成剩余的n-2条边
-            node_1 = choice(list(used_nodes))
-            node_2 = choice(list(unused_nodes))
-            used_nodes.add(node_2)
-            unused_nodes.remove(node_2)
-            if node_2 < node_1:
-                node_1, node_2 = node_2, node_1
-            edges[node_1][node_2]=randint(min_w, max_w)
-            node_pair.add((node_1, node_2))
-            edge_size += 1
+    # 保证图的连通性，先生成一个包含所有节点的随机环
+    shuffle(nodes)
+    for i in range(node_num):
+        node_1 = nodes[i]
+        node_2 = nodes[(i + 1) % node_num]
+        weight = randint(min_w, max_w)
+        edges[node_1][node_2] = weight
+        node_pair.add((node_1, node_2))
+        edge_size += 1
 
-    # 添加剩余的m-n+1条边
+    # 添加剩余的边直到达到edge_num
     while edge_size < edge_num:
-        node_1 = randint(0, node_num-2)
-        node_2 = randint(node_1+1, node_num-1)
-        if (node_1, node_2) not in node_pair:
-            edges[node_1][node_2]=randint(min_w, max_w)
+        node_1 = randint(0, node_num - 1)
+        node_2 = randint(0, node_num - 1)
+        if node_1 != node_2 and (node_1, node_2) not in node_pair:
+            weight = randint(min_w, max_w)
+            edges[node_1][node_2] = weight
             node_pair.add((node_1, node_2))
-            edge_size+=1
+            edge_size += 1
 
     return edges
 
 def get_negative_edges(node_num: int,edges: list) \
     -> list:
-    # 随机生成负权环的顶点
-    cycle_nodes=sample(range(0, node_num),2)
-    node_1=cycle_nodes[0]
-    node_2=cycle_nodes[1]
-    edges[node_1][node_2]=-1
-    edges[node_2][node_1]=0
-    #print(node_1,node_2,edges[node_1][node_2])
+    new_edges = copy.deepcopy(edges)
 
-    return edges
+    for _ in range(randint(1, 20)):
+        while True:
+            temp_edges = copy.deepcopy(new_edges)
+            cycle_nodes = sample(range(0, node_num), 2)
+
+            node_1 = cycle_nodes[0]
+            node_2 = cycle_nodes[1]
+            temp_edges[node_1][node_2] = randint(-20, -1)
+
+            result_edgeslist = []
+            for i in range(node_num):
+                for j in range(node_num):
+                    if temp_edges[i][j] != 0:
+                        result_edgeslist.append((i, j, temp_edges[i][j]))
+
+            if bellman_ford(node_num, 0, result_edgeslist) is not None:
+                new_edges = temp_edges
+                break
+    return new_edges
 
 def get_negative_cycle(node_num: int,edges: list) \
     -> list:
@@ -114,7 +115,7 @@ def get_random_input(node_num: int, edge_num: int,
 
     min_weight=2
     max_weight=20
-    #先生成一个不含负权边的无向连通图
+    #先生成一个不含负权边的有向连通图
     edges = get_random_graph(node_num, edge_num, min_weight, max_weight)
 
     if has_negative_cycle:
@@ -150,8 +151,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     for _ in range(args.num):
         node_number = randint(args.min_node, args.max_node)
-        min_edge_number = max(args.min,node_number)
-        max_edge_number = min(min_edge_number+1,args.max,node_number*(node_number-1)/2)
+        max_edge_number = min(args.min,args.max,node_number*(node_number-1)/2)
+        min_edge_number = min(args.min,node_number,max_edge_number)
         edge_number = randint(min_edge_number,max_edge_number)
         edge_list = get_random_input(
             node_number, edge_number, args.has_negative_edges, args.has_negative_cycle)
